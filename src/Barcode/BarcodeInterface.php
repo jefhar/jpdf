64c8caf6eb0a7ec30d8fa424ccae96eb3a31e542
Jeff Harris <jeff@jeffharris.us>
2021-08-28 15:12:46 -0400

    Add more constants.

--- src/Barcode/BarcodeInterface.php
+++ src/Barcode/BarcodeInterface.php
@@ -6,9 +6,18 @@ interface BarcodeInterface
 {
     // Array keys
     public const BCODE = 'bcode';
+    public const CHECKDIGIT = 'checkdigit';
     public const CODE = 'code';
+    public const LIGHTML = 'lightmL';
+    public const LIGHTMR = 'lightmR';
     public const MAXH = 'maxh';
     public const MAXW = 'maxw';
+    public const NOMH = 'nom-H';
+    public const NOMX = 'nom-X';
+    public const QUIETL = 'quietL';
+    public const QUIETR = 'quietR';
+    public const QUIETTB = 'quietTB';
+    public const SEPM = 'sepM';
 
     public function getType(): string;
 
